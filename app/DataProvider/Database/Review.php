cc70885367a87a9d3eacd64a0c9bb5dbb0b3766e
natsume0718 <moto718program@gmail.com>
2019-08-02 00:06:51 +0900

    Eloquentモデル作成

---
+++ app/DataProvider/Database/Review.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\DataProvider\Database;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Review extends Model
+{
+    //
+}
---
+++ app/DataProvider/Database/Tag.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\DataProvider\Database;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tag extends Model
+{
+    //
+}
