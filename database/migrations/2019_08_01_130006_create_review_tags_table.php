1646156173c1eb3d13c0bde32c54dd3e7ce4ed16
natsume0718 <moto718program@gmail.com>
2019-08-01 22:28:40 +0900

    [add] マイグレーション追加

---
+++ database/migrations/2019_08_01_130006_create_review_tags_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReviewTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('review_tags', function (Blueprint $table) {
+            $table->integer('review_id', false, true);
+            $table->integer('tag_id', false, true);
+            $table->softDeletes();
+            $table->timestamps();
+            $table->unique(['review_id', 'tag_id'], 'UNIQUE_REVIEW_TAGS');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('review_tags');
+    }
+}
